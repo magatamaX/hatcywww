174b18b79b9855109ee9647798cfcf214002daee
日崎智博 <magatama.club@gmail.com>
2019-11-03 12:14:26 +0900

    11/4お知らせ追加

--- components/index/Kv/index.tsx
+++ components/index/Kv/index.tsx
@@ -1,8 +1,15 @@
 import css from './style.scss'
+import Link from 'next/link'
 
 const Kv = ({ isAniKvDone } : { isAniKvDone: boolean }) => (
     <div className={css.kv}>
         <div className={css.inner}>
+			<div className={css.event}>
+				<Link as="/information/p/11-4" href="/post?slug=11-4">
+					<figure><img src="/static/images/chirashi.jpg" alt="島はっちー単独公演vol.4 &amp; 5『銀河鉄道ノ刻』" /></figure>
+				</Link>
+				<p>11月4日 ご予約受付中！</p>
+			</div>
             <p className={`${css.title} ${isAniKvDone ? css.on : ''}`}>
                 <img src="/static/images/index/kv/title.png" alt="パフォーミングアーティスト・徳島はっちー" />
             </p>
